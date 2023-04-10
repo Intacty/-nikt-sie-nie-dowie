e92bf96e941d394016d7f93074879a955fc0dd5f
geting <67223233+Intacty@users.noreply.github.com>
2023-04-10 20:46:59 +0200

    Add files via upload

--- keydrop.py
+++ keydrop.py
@@ -8,19 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 import json
 import time
 
-driver_path = 'C:/Users/geting/Downloads/chromedriver_win32/chromedriver.exe'
+driver_path = 'chromedriver.exe'
 
 service = Service(executable_path=driver_path)
 service.start()
 
 options = Options()
-options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-setuid-sandbox')
 options.add_argument('--no-sandbox')
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+chrome_options.add_experimental_option("prefs", {"profile.content_settings.exceptions.notifications": {"https://key-drop.com": 2}})
 
 driver = webdriver.Chrome(service=service, options=options)
 
@@ -30,10 +30,11 @@ with open('cookies.json', 'r') as f:
 while True:
     driver.get('https://key-drop.com/en/giveaways/list')
 
+
     for cookie in cookies:
         driver.add_cookie(cookie)
 
-    time.sleep(10)
+    time.sleep(5)
     find_giveaway = driver.find_element(By.CSS_SELECTOR, 'div.relative:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)')
     find_giveaway.click()
     print("[CONSOLE] Giveaway został znaleziony")
@@ -42,15 +43,15 @@ while True:
     join_giveaway = driver.find_element(By.CSS_SELECTOR, 'div.relative:nth-child(4) > div:nth-child(1)')
     join_giveaway.click()
     print("[CONSOLE] Dołączono do giveawaya")
-    
-    time.sleep(5)
-    driver.refresh()
+
+    driver.execute_script("document.body.style.zoom='75%'")
 
     try:
         wait = WebDriverWait(driver, 999)
-        first_giveaway = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "li.relative:nth-child(1) > div:nth-child(1)")))
+        first_giveaway = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#giveaway-drawing-winners")))
         print("[CONSOLE] Wygrana osoba została wylosowana, wracam do strony głównej")
     except:
         print("CSS selector not found")
 
-    time.sleep(5)
+    driver.refresh()
+    time.sleep(20)
