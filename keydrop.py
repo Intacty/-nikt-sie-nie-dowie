ef16d91e6720a26a8a5539c2ebe8c8efa8a2768b
geting <67223233+Intacty@users.noreply.github.com>
2023-04-14 19:14:41 +0200

    Update keydrop.py

--- keydrop.py
+++ keydrop.py
@@ -5,25 +5,25 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import requests
 import json
 import time
 import datetime
 import undetected_chromedriver as uc
-import sys
 
 options = uc.ChromeOptions()
 options=options
 
-driver = uc.Chrome(
-    options = options , version_main = 112
-    )
-
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-
+options.add_argument('--load-extension={}'.format(r'C:\Users\geting\AppData\Local\Google\Chrome\User Data\Default\Extensions\ifibfemgeogfhoebkmokieepdoobkbpo\3.3.3_0'))
+options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument('--lang=pl-PL')
 options.add_argument('--disable-tracking')
-options.add_argument("--timezone=Poland/Warsaw")
+options.add_argument('--timezone=Poland/Warsaw')
+
+driver = uc.Chrome(options = options , version_main = 112)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
 
 stealth(driver,
         languages=["pl-PL", "PL"],
@@ -34,8 +34,6 @@ stealth(driver,
         fix_hairline=True,
         )
 
-window_handle = driver.window_handles[0]
-driver.switch_to.window(window_handle)
 driver.set_window_position(0, 0)
 screen_width = driver.execute_script("return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;")
 screen_height = driver.execute_script("return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;")
@@ -44,18 +42,17 @@ driver.set_window_size(screen_width // 2, screen_height)
 def internal_error(driver, cookies):
     try:
         wait = WebDriverWait(driver, 3)
-        element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#giveaways-root > div > div > div > button > span")))
-        print("[CONSOLE] Strona jest zbugowana jak skurwynsyn i wystartowała z błędem BRUH")
-        driver.refresh()
-        driver.refresh()
-        driver.refresh()
-        driver.refresh()
-        driver.refresh()
-        driver.refresh()
-        driver.refresh()
+        while True:
+            try:
+                element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#giveaways-root > div > div > div > button > span")))
+                print("[CONSOLE] Strona jest zbugowana jak skurwynsyn i wystartowała z błędem BRUH")
+                driver.refresh()
+            except TimeoutException:
+                print("[CONSOLE] Strona działa poprawnie")
+                break
         return
     except Exception as e:
-        print("[CONSOLE] Strona działa poprawnie")
+        print("[CONSOLE] Wystąpił inny błąd:", e)
 
 def drawing_is_started(driver, cookies):
     try:
@@ -66,50 +63,58 @@ def drawing_is_started(driver, cookies):
         return
     except:
         print("[CONSOLE] Giveaway się jeszcze nie rozpoczął")
- 
+
 def find_giveaway(driver, cookies):
     while True:
         max_retries = 1
         retries = 0
+        found_giveaway = False
 
         while retries < max_retries:
             try:
-                find_giveaway = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/main/div/div/div/div[3]/div[2]/div[5]/div/div/div/div[2]/a')))
+                find_giveaway = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/main/div/div/div/div[3]/div[2]/div[5]/div/div/div/div[2]/a')))
                 find_giveaway.click()
                 print("[CONSOLE] [{}] Giveaway został znaleziony".format(datetime.datetime.now().strftime("%H:%M:%S")))
+                found_giveaway = True
                 break
             except Exception as e:
                 print("[CONSOLE] [{}] Błąd podczas znajdowania giveawaya".format(str(e)))
                 retries += 1
 
-        if retries == max_retries:
-            print("[CONSOLE] Nie udało się znaleźć giveawaya, odswiezam stronę".format(max_retries))
-            driver.get('https://key-drop.com/pl/giveaways/list')
-            time.sleep(2)
-            driver.refresh()
-        return
+            if retries == max_retries:
+                print("[CONSOLE] Nie udało się znaleźć giveawaya, odswiezam stronę".format(max_retries))
+                driver.get('https://key-drop.com/pl/giveaways/list')
+                driver.refresh()
+                retries = 0
+            return
+        if found_giveaway:
+            break
 
 def join_giveaway(driver, cookies):
     while True:
         max_retries = 1
         retries = 0
+        joined_giveaway = False
 
         while retries < max_retries:
             try:
-                join_giveaway = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.button')))
+                join_giveaway = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div > div.flex.flex-col.gap-4 > div.flex.flex-1.flex-col > div.relative > div > button > svg')))
                 join_giveaway.click()
                 print("[CONSOLE] [{}] Dołączono do giveawaya".format(datetime.datetime.now().strftime("%H:%M:%S")))
+                joined_giveaway = True
                 break
             except Exception as e:
                 print("[CONSOLE] [{}] Błąd podczas dołączania do giveawaya".format(str(e)))
                 retries += 1
 
-        if retries == max_retries:
-            print("[CONSOLE] Nie udało się dołączyć do giveawaya, odswiezam stronę".format(max_retries))
-            driver.get('https://key-drop.com/pl/giveaways/list')
-            time.sleep(2)
-            driver.refresh()
-        return
+            if retries == max_retries:
+                print("[CONSOLE] Nie udało się dołączyć do giveawaya, odswiezam stronę".format(max_retries))
+                driver.get('https://key-drop.com/pl/giveaways/list')
+                driver.refresh()
+                retries = 0
+            return
+        if joined_giveaway:
+            break
 
 def ended_giveaway(driver, cookies):
     while True:
@@ -118,18 +123,17 @@ def ended_giveaway(driver, cookies):
 
         while retries < max_retries:
             try:
-                wait = WebDriverWait(driver, 999)
-                ended_giveaway = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#giveaway-drawing-winners")))
+                wait = WebDriverWait(driver, 900)
+                ended_giveaway = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#giveaway-drawing-winners > h3")))
                 print("[CONSOLE] [{}] Wygrana osoba została wylosowana, wracam do strony głównej".format(datetime.datetime.now().strftime("%H:%M:%S")))
                 break
             except Exception as e:
-                print("[CONSOLE] [{}] Nie udało się wyłonić zwycięzcy: {}".format(str(e)))
+                print("[CONSOLE] Nie udało się wyłonić zwycięzcy".format(str(e)))
                 retries += 1
 
         if retries == max_retries:
-            print("[CONSOLE] [{}] Nie udało się wyłonić zwycięzcy, odswiezam stronę".format(max_retries))
+            print("[CONSOLE] Nie udało się wyłonić zwycięzcy, odswiezam stronę".format(max_retries))
             driver.get('https://key-drop.com/pl/giveaways/list')
-            time.sleep(2)
             driver.refresh()
         return
 
@@ -143,17 +147,10 @@ while True:
         driver.add_cookie(cookie)
 
     internal_error(driver, cookies)
-
     drawing_is_started(driver, cookies)
-
-    time.sleep(3)
-
+    time.sleep(5)
     find_giveaway(driver, cookies)
     driver.refresh()
-
     join_giveaway(driver, cookies)
-
     ended_giveaway(driver, cookies)
-    driver.refresh()
-
     time.sleep(10)
